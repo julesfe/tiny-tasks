093a63ad928e710e8d2cc79b7bfb8ac850ea936f
Jules <fehr.jules@gmail.com>
2019-09-24 12:54:16 +0200

    [Add] password encoder

--- src/main/java/com/coyoapp/tinytask/configuration/WebSecurityConfig.java
+++ src/main/java/com/coyoapp/tinytask/configuration/WebSecurityConfig.java
@@ -1,24 +1,20 @@
 package com.coyoapp.tinytask.configuration;
 
-import com.coyoapp.tinytask.service.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  UserDetailsServiceImpl userDetailsService;
-
-  @Override
-  protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
   @Override
