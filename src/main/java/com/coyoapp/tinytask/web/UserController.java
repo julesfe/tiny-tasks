a9f76dc0005b9e1029057f28392d4d8d8896cbd4
Jules <fehr.jules@gmail.com>
2019-10-08 17:23:08 +0200

    [Add] register and confirm controller endpoints

--- src/main/java/com/coyoapp/tinytask/web/UserController.java
+++ src/main/java/com/coyoapp/tinytask/web/UserController.java
@@ -5,6 +5,7 @@ import com.coyoapp.tinytask.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +21,19 @@ public class UserController {
   @PostMapping("/login")
   public boolean login(@RequestBody User user) {
     log.debug("login User {}", user);
-    userEntity = userRepository.findByUsername(user.getUsername()).orElseThrow(
+    User userEntity = userRepository.findByUsername(user.getUsername()).orElseThrow(
       () -> new ResponseStatusException(HttpStatus.UNAUTHORIZED,
         "could not login user with username " + user.getUsername()));
     return user.getPassword().equals(userEntity.getPassword());
+  }
+
+  @PostMapping("/register")
+  public void registration(@RequestBody User user) {
+    log.debug("registering User {}", user);
+  }
+
+  @GetMapping("/confirm")
+  public void confirm(@RequestBody User user) {
+    log.debug("confirming User {}", user);
   }
 }
