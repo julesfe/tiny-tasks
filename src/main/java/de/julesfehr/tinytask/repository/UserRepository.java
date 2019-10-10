39367c3a5caec160bc9fe947797f5f474f988abc
Jules <fehr.jules@gmail.com>
2019-10-10 11:59:32 +0200

    :art: added findByEmail and saveUser to a renamed UserService

--- src/main/java/de/julesfehr/tinytask/repository/UserRepository.java
+++ src/main/java/de/julesfehr/tinytask/repository/UserRepository.java
@@ -10,6 +10,8 @@ public interface UserRepository extends JpaRepository<User, String> {
 
   Optional<User> findById(String id);
 
+  Optional<User> findByEmail(String email);
+
   Optional<User> findByUsername(String username);
 
 }
