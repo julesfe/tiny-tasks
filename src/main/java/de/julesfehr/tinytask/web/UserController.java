990b2a5e2e22830c07b3b8490fa4c3e726f9e474
Jules <fehr.jules@gmail.com>
2019-10-19 00:40:50 +0200

    :sparkles: added submit registration form method

--- src/main/java/de/julesfehr/tinytask/web/UserController.java
+++ src/main/java/de/julesfehr/tinytask/web/UserController.java
@@ -2,10 +2,12 @@ package de.julesfehr.tinytask.web;
 
 import de.julesfehr.tinytask.domain.User;
 import de.julesfehr.tinytask.service.UserService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,9 +25,34 @@ public class UserController {
 
   @GetMapping("/register")
   public ModelAndView showRegistrationForm(ModelAndView modelAndView, User user) {
-    log.debug("registering User {}", user);
+    log.debug("showing registration form for");
     modelAndView.addObject("user", user);
     modelAndView.setViewName("register");
+    return modelAndView;
+  }
+
+  @PostMapping("/register")
+  public ModelAndView submitRegistrationForm(ModelAndView modelAndView, @Valid User user,
+    BindingResult bindingResult) {
+    log.debug("processing registration form for user {}", user.getEmail());
+
+    try {
+      User userExists = userService.findByEmail(user.getEmail());
+      log.debug("user {} already exists", user.getEmail());
+      modelAndView.addObject("userExistsMessage", "A user with that email already exists");
+      modelAndView.setViewName("register");
+    } catch (Exception e) {
+      log.debug("user {} does not already exist", user.getEmail());
+    }
+    if (bindingResult.hasErrors()) {
+      modelAndView.setViewName("register");
+    } else {
+      log.debug("saving user {}", user);
+      userService.saveUser(user);
+      modelAndView.addObject("confirmationMessage", "registration successful");
+      modelAndView.setViewName("register");
+      log.debug("user {} completed the registration process", user.getEmail());
+    }
     return modelAndView;
   }
 
--- src/test/java/de/julesfehr/tinytask/tinytask/web/UserControllerTest.java
+++ src/test/java/de/julesfehr/tinytask/tinytask/web/UserControllerTest.java
@@ -1,7 +1,10 @@
 package de.julesfehr.tinytask.tinytask.web;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -16,7 +19,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.web.servlet.ModelAndView;
 
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class UserControllerTest extends BaseControllerTest {
@@ -34,16 +36,61 @@ public class UserControllerTest extends BaseControllerTest {
   }
 
   @Test
+  public void should_add_user_exists_message_to_model_when_user_exists() throws Exception {
+    String username = "test";
+    String password = "hunter2";
+    String email = "test@testmail.de";
+    User user = new User(1, email, username, password, null);
+    given(userService.findByEmail(anyString())).willReturn(user);
+
+    ResultActions actualResult = this.mockMvc.perform(post(PATH_REGISTRATION)
+      .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+      .content("username=" + username + "&email=" + email + "&password=x&ConfirmPassword=x"));
+
+    actualResult.andExpect(ResultMatcher.matchAll(
+      model().attributeExists("userExistsMessage")));
+  }
+
+  @Test
+  public void should_save_user_when_user_did_not_exist_before() throws Exception {
+    String username = "test";
+    String email = "test@testmail.de";
+    String password = "password";
+    User user = new User(0, email, username, password, null);
+    given(userService.findByEmail(any())).willThrow(new EntityNotFoundException());
+
+    ResultActions actualResult = this.mockMvc.perform(post(PATH_REGISTRATION)
+      .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+      .content("username=" + username + "&email=" + email + "&password=" + password));
+
+    verify(userService, times(1)).saveUser(user);
+  }
+
+  @Test
+  public void should_add_confirmation_message_to_model_when_user_has_been_saved() throws Exception {
+    String username = "test";
+    String email = "test@testmail.de";
+    given(userService.findByEmail(any())).willThrow(new EntityNotFoundException());
+
+    ResultActions actualResult = this.mockMvc.perform(post(PATH_REGISTRATION)
+      .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+      .content("username=" + username + "&email=" + email + "&password=x&ConfirmPassword=x"));
+
+    actualResult.andExpect(ResultMatcher.matchAll(
+      model().attributeExists("confirmationMessage")));
+  }
+
+  @Test
   public void should_return_status_code_200_when_login_was_successful() throws Exception {
     String username = "test";
     String password = "hunter2";
     String email = "test@testmail.de";
-    User user = new User(null, email, username, password, null);
+    User user = new User(1, email, username, password, null);
     given(userService.findByUsername(anyString())).willReturn(user);
 
     ResultActions actualResult = this.mockMvc.perform(post(PATH_LOGIN)
       .contentType(MediaType.APPLICATION_JSON_UTF8)
-      .content("{\"username\":\"test\",\"password\":\"hunter2\"}"));
+      .content("{\"username\":\"test\",\"email\":\"test\",\"password\":\"hunter2\"}"));
 
     actualResult.andExpect(status().isOk());
   }
@@ -64,7 +111,7 @@ public class UserControllerTest extends BaseControllerTest {
     String username = "test";
     String password = "hunter2";
     String email = "test@testmail.de";
-    User user = new User(null, email, username, password, null);
+    User user = new User(1, email, username, password, null);
 
     ResultActions actualResult = this.mockMvc.perform(post(PATH_REGISTRATION)
       .contentType(MediaType.APPLICATION_JSON_UTF8)
