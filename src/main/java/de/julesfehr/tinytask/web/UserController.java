3e8bc841ab5c1cbe944ae9cae74875b39ed915b0
Jules <fehr.jules@gmail.com>
2019-10-18 17:22:27 +0200

    :sparkles: registration page with thymeleaf

--- src/main/java/de/julesfehr/tinytask/web/UserController.java
+++ src/main/java/de/julesfehr/tinytask/web/UserController.java
@@ -5,7 +5,6 @@ import de.julesfehr.tinytask.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
