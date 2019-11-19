4cca85154a2a974b1cd4db4676f105bdfcd9cae7
Jules <fehr.jules@gmail.com>
2019-11-19 21:05:08 +0100

    :fire: removed service worker

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React, {PureComponent} from 'react'
+
+import './App.css'
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div styleName="root">
+        ROOT COMPONENT
+      </div>
+    )
+  }
+}
+
+export default App
