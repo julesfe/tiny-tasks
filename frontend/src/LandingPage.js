fa201921681fecee3e3199ed4f1b79111bbd3064
Jules <fehr.jules@gmail.com>
2019-11-20 21:56:46 +0100

    :sparkles: added appbar

---
+++ frontend/src/LandingPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function LandingPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <a
+          className="App-link"
+          href="/tasks"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Enter TinyTasks
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
