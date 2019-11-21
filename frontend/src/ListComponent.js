531e2bf194e717425b3688dfd499ab6687ca5030
Jules <fehr.jules@gmail.com>
2019-11-21 22:51:54 +0100

    :recycle: changed the list from function to class component

--- frontend/src/ListComponent.js
+++ frontend/src/ListComponent.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import StarIcon from '@material-ui/icons/Star';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,20 +13,22 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function ListComponent() {
-  const classes = useStyles();
+export default class ListComponent extends PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+        <List component="nav" aria-label="contacts">
+          <ListItem button>
+            <ListItemIcon>
+            </ListItemIcon>
+            <ListItemText primary="Chelsea Otakan"/>
+          </ListItem>
+          <ListItem button>
+            <ListItemText inset primary="Eric Hoffman"/>
+          </ListItem>
+        </List>
+      </React.Fragment>
+    );
+  }
 
-  return (
-    <List component="nav" className={classes.root} aria-label="contacts">
-      <ListItem button>
-        <ListItemIcon>
-          <StarIcon/>
-        </ListItemIcon>
-        <ListItemText primary="Chelsea Otakan"/>
-      </ListItem>
-      <ListItem button>
-        <ListItemText inset primary="Eric Hoffman"/>
-      </ListItem>
-    </List>
-  );
 }
